46b15c471fdd9d0657b39dc33fe61bb888fd37e2
Tdallau <tim@dallau.com>
2020-09-03 14:04:33 +0200

    first commit

---
+++ src/app/_models/event.ts
@@ -0,0 +1,8 @@
+export interface Event {
+  SUMMARY: string;
+  DTSTAMP: string;
+  DTSTART: string;
+  DESCRIPTION: string;
+  LOCATION: string;
+  DURATION: string;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Event} from './_models/event';
+import * as moment from 'moment-timezone';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'mycalenderFrontend';
+export class AppComponent implements OnInit  {
+  constructor(private http: HttpClient) {}
+  events: Event[] = []
+  ngOnInit(): void {
+    this.http.get<Event[]>('https://mycalender.dallau.com/api/calender/data/1').subscribe(events => {
+      console.log(events)
+      this.events = events;
+    })
+  }
+
+  printDate(date: string) {
+    const convert = moment.tz(date, "Europe/Paris");
+    return `${convert.date()}-${convert.month() + 1}-${convert.year()} ${convert.hour()}:${convert.minute()}`;
+  }
+  
 }
